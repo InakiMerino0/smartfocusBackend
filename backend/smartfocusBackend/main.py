8e1eca13071e549308088b2632cc597984fee855
Inaki Merino <105252175+InakiMerino0@users.noreply.github.com>
2025-10-03 18:07:05 -0300

    Corrected main.py

--- backend/smartfocusBackend/main.py
+++ backend/smartfocusBackend/main.py
@@ -1,14 +1,15 @@
-import logging
-
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Depends, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import JSONResponse
-from fastapi.exceptions import RequestValidationError
+from sqlalchemy.orm import Session
+from .database import get_db
+from sqlalchemy import text
+from .routers import v1_auth, v1_events, v1_nl, v1_subjects, v1_users
 
-
-app = FastAPI(title="SmartFocus Backend", version="1.0")
-
-logger = logging.getLogger("smartfocus")
+app = FastAPI(
+    title="SmartFocus Backend",
+    description="Autenticación de usuarios, gestión de eventos y materias con IA",
+    version="1.0"
+)
 
 app.add_middleware(
     CORSMiddleware,
@@ -18,3 +19,8 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+app.include_router(v1_auth.router)
+app.include_router(v1_events.router)
+app.include_router(v1_nl.router)  
+app.include_router(v1_subjects.router)
+app.include_router(v1_users.router)
